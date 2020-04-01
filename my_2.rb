10c7722bc6a2a225f7a8fb53dc5c9f0771d6dced
Tretyak Serhiy <38050454+TretyakSerhiy@users.noreply.github.com>
2020-04-01 14:02:46 +0300

    Create my_2.rb

---
+++ my_2.rb
@@ -0,0 +1,15 @@
+#Задача №2
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+index = 0
+x = []
+y = []
+for a[index] in a
+  if index % 2 != 0
+    x << a[index]
+  else
+    y << a[index]
+  end
+    index += 1
+end
+c = x+y
+print c
