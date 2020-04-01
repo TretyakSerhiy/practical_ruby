38d5435156e48edd23d82911d744295803f1212e
Tretyak Serhiy <38050454+TretyakSerhiy@users.noreply.github.com>
2020-04-01 17:23:17 +0300

    Create my_24.rb

---
+++ my_24.rb
@@ -0,0 +1,17 @@
+=begin
+Задача №24: дано вещественное число R и массив вещественных чисел.
+Найти элемент массива, который наименее близок к данному числу.
+=end
+a = [-9.1, -7.65, 32.64, 96.25, 52.21, -34.12, 57.12, 1.67]
+R = 9.8
+x = (R - a[0]).abs
+amount = 0.0
+index = 0
+for a[index] in a
+    if x < (R - a[index].to_f).abs
+      x = (R - a[index].to_f).abs
+      amount = a[index]
+    end
+      index+=1
+end
+puts amount
