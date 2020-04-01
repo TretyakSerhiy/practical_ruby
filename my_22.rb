879fba2accde47f74ee715a31ca2c9c30a936651
Tretyak Serhiy <38050454+TretyakSerhiy@users.noreply.github.com>
2020-04-01 17:18:31 +0300

    Create my_22.rb

---
+++ my_22.rb
@@ -0,0 +1,18 @@
+=begin
+Задача №22: дан целочисленный массив.
+Определить количество участков, на которых его элементы монотонно убывают.
+=end
+a = [9, 8, 7, 6, 32, 64, 96, 25, 52]
+x = 0
+amount = 0
+index = 0
+for a[index] in a
+    if a[index+1].to_i - a[index].to_i == a[index].to_i - a[index-1].to_i && a[index+1].to_i - a[index].to_i < 0
+      if x != a[index].to_i - a[index-1].to_i
+        x = a[index+1].to_i - a[index].to_i
+        amount += 1
+      end
+    end
+  index+=1
+end
+puts amount
